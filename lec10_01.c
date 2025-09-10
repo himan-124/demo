0033a62ab42b208a8730a3ca929663f7948f8fe1
himan-124 <hk2154064@gmail.com>
2025-09-10 15:47:45 +0530

    msg

---
+++ lec10_01.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+int main(){
+    for (int i=0;i<4;i++){
+        for (int j=0;j<=i;j++){
+            printf("*");
+        }
+        for(int p=3;p>i;p--){
+            printf(" ");
+        }
+         for (int p=3;p>i;p--){
+            printf(" ");
+         }
+        for(int p=0;p<=i;p++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+    for (int i=0;i<4;i++){
+        for (int j=3;j>i;j--){
+             printf("*");
+        }
+        for (int p=0;p<=i;p++){
+            printf(" ");
+        }
+         for(int p=0;p<=i;p++){
+            printf(" ");
+         } 
+         for (int j=3;j>i;j--){
+            printf("*");
+         }
+         printf("\n");
+        
+        
+    }
+}
+/*
+
+
+*      *
+**    **
+***  ***
+********
+***  ***
+**    **
+*      *
+            
+*/
---
+++ lec10_02.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    for (int i=0;i<=3;i++){//condition check for 0 then for 1 then for 2and at the time of 3 the condition is flse
+        for(int j=0;j<=3;j++){//condition check for the next line bole toh j row read krke condition apply krenge  
+            if (i==0 || i==3 || j==0 || j==3){//if true toh * print hoga 
+                printf("*");
+
+            } else{//if condition is false toh space print hogi
+                printf(" ");
+            }
+           // printf("*");
+        }
+         
+        printf("\n");//next row pe jane ke liye
+    }
+}
+/*
+
+
+****
+*  *
+*  *
+****
+              
+
+*/
