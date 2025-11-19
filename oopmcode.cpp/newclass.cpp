d4cf1a1d1703a0cdb84e9080ed27ddff1ac860de
himan-124 <hk2154064@gmail.com>
2025-11-19 10:42:10 +0530

    msg

---
+++ oopmcode.cpp/newclass.cpp
@@ -0,0 +1,67 @@
+/*#include <iostream>
+using namespace std;
+class Animal{
+    public:
+    virtual void makesound(){
+        cout <<"Animal makes sound\n";
+
+    }
+};
+class Dog:public Animal {
+    public:
+    void makesound (){
+        cout <<"Bhauuubhaauuu!!!!\n";
+    }
+};
+class Cat: public Animal {
+    public:
+    void makesound (){
+        cout<<"meooowwwwwmeooowwwww!!!!!!\n";
+    }
+};
+int main (){
+    Animmal*a=new Animal[4];
+    a[0]=new Dog;
+    a[1]=new Cat;
+    a[2]=new Dog;
+    a[4]=new Cat;
+
+}*/
+#include <iostream>
+using namespace std;
+
+class Animal {
+public:
+    virtual void makesound() {
+        cout << "Animal makes sound\n";
+    }
+};
+
+class Dog : public Animal {
+public:
+    void makesound() override {
+        cout << "Bhauuubhaauuu!!!!\n";
+    }
+};
+
+class Cat : public Animal {
+public:
+    void makesound() override {
+        cout << "meooowwwwwmeooowwwww!!!!!!\n";
+    }
+};
+
+int main() {
+    Animal* a[4];
+    a[0] = new Dog;
+    a[1] = new Cat;
+    a[2] = new Dog;
+    a[3] = new Cat;
+
+    for (int i = 0; i < 4; ++i) {
+        a[i]->makesound();
+        delete a[i]; // Clean up memory
+    }
+
+    return 0;
+}
