d2ada29723e73249685f9162aa590a1ec5ee6630
himan-124 <hk2154064@gmail.com>
2025-11-19 15:57:15 +0530

    msg

---
+++ c_training/lec1_02.c
@@ -0,0 +1,14 @@
+//allot the values and print the values 
+#include <stdio.h>
+
+int main(){
+    int age = 20;
+    float pi = 3.14;
+    char grade = 'A';
+    printf("Hello Students...!\n");
+    printf("Age = %d\n",age);
+    printf("pi = %f\n",pi);
+    printf("Grade = %c\n",grade);
+    return 0;
+
+}
---
+++ c_training/lec1_03.c
@@ -0,0 +1,11 @@
+//wap to taking input from the user
+#include <stdio.h>
+
+int main(){
+    
+    int a;
+    printf("enter the number: ");
+    scanf("%d",&a);
+    printf("you entered %d \n\n",a);
+    return 0;
+}
