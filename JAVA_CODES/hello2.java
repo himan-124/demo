0d7d86a68f483096f2bbe0b3de6065de9beb3cfd
himan-124 <hk2154064@gmail.com>
2025-12-08 15:13:37 +0530

    msg

--- JAVA_CODES/hello2.java
+++ JAVA_CODES/hello2.java
@@ -5,14 +5,16 @@ public class hello2 {
         list.add(5);
         list.add(6);
         list.add(7);
+        list.add(8);
 
         Iterator<Integer> it = list.iterator();
         while (it.hasNext()) {
             int currentElement = it.next(); // Store the current element
-            System.out.println(currentElement);
+            //System.out.println(currentElement);
             if (currentElement == 6) { // Use '==' for comparison
                 it.remove();
             }
 }
+System.out.println(list);
 }
 }
